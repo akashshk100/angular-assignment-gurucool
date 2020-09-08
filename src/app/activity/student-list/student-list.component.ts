a9477d22ddc9567e25fff14abefe9f201e56fdc2
akash shukla <shuklaji.2227@gmail.com>
2020-09-08 10:58:12 +0530

    offline student list added

--- src/app/activity/student-list/student-list.component.ts
+++ src/app/activity/student-list/student-list.component.ts
@@ -23,7 +23,128 @@ export class StudentListComponent implements OnInit ,AfterViewInit{
 
     }
 
-    STUDENT_DATA: Student[] = [];
+    STUDENT_DATA: Student[] = [
+        {
+          "position": 1,
+          "name": "Emery Jaskolski",
+          "attendance": 85,
+          "score": 62
+        },
+        {
+          "position": 2,
+          "name": "Antonetta Klocko",
+          "attendance": 6,
+          "score": 46
+        },
+        {
+          "position": 3,
+          "name": "Jeremie Simonis",
+          "attendance": 96,
+          "score": 33
+        },
+        {
+          "position": 4,
+          "name": "Hermina Pfannerstill",
+          "attendance": 89,
+          "score": 78
+        },
+        {
+          "position": 5,
+          "name": "Selmer Jast",
+          "attendance": 19,
+          "score": 65
+        },
+        {
+          "position": 6,
+          "name": "Colt Christiansen II",
+          "attendance": 89,
+          "score": 24
+        },
+        {
+          "position": 7,
+          "name": "Jasmin Wisoky",
+          "attendance": 68,
+          "score": 17
+        },
+        {
+          "position": 8,
+          "name": "Dorian Goodwin",
+          "attendance": 50,
+          "score": 88
+        },
+        {
+          "position": 9,
+          "name": "Wilford Bergstrom",
+          "attendance": 45,
+          "score": 82
+        },
+        {
+          "position": 10,
+          "name": "Torrance Ernser",
+          "attendance": 70,
+          "score": 98
+        },
+        {
+          "position": 11,
+          "name": "Monserrate Prosacco",
+          "attendance": 84,
+          "score": 75
+        },
+        {
+          "position": 12,
+          "name": "Litzy Ratke II",
+          "attendance": 29,
+          "score": 28
+        },
+        {
+          "position": 13,
+          "name": "Desmond Dare",
+          "attendance": 39,
+          "score": 35
+        },
+        {
+          "position": 14,
+          "name": "Dr. Barney Strosin",
+          "attendance": 92,
+          "score": 39
+        },
+        {
+          "position": 15,
+          "name": "Hassan Altenwerth",
+          "attendance": 95,
+          "score": 31
+        },
+        {
+          "position": 16,
+          "name": "Deja Braun",
+          "attendance": 35,
+          "score": 81
+        },
+        {
+          "position": 17,
+          "name": "Joey Eichmann IV",
+          "attendance": 49,
+          "score": 72
+        },
+        {
+          "position": 18,
+          "name": "Cordie Lakin",
+          "attendance": 2,
+          "score": 98
+        },
+        {
+          "position": 19,
+          "name": "Haskell Purdy",
+          "attendance": 74,
+          "score": 32
+        },
+        {
+          "position": 20,
+          "name": "Kara Jenkins",
+          "attendance": 47,
+          "score": 7
+        }
+      ];
     dataSource
     isLoading: boolean = true
     httpError: boolean = false
@@ -37,8 +158,11 @@ export class StudentListComponent implements OnInit ,AfterViewInit{
             this.dataSource.paginator = this.paginator;
         },
         (errorResponse) => {
-            this.httpError = true
-            this.openSnackBar("Mock API limit reached, try again later")
+            this.isLoading = false
+            this.dataSource = new MatTableDataSource<Student>(this.STUDENT_DATA);
+            this.dataSource.paginator = this.paginator;
+            // this.httpError = true
+            // this.openSnackBar("Mock API limit reached, try again later")
         })
     }
 
